780e79773cf5984260e21ce89848f697ca770f69
rob.chartier <rob.chartier@94cf2702-d094-11de-a306-d16691e8d855>
2010-01-14 22:15:51 +0000

    Initial checking for a command line tool to send notifications to XBMC upon download complete. XBMCNotify.exe /? for help on the command.

---
+++ XBMCNotify/XBMCNotify/Args.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Unified.CommandLine;
+
+namespace XBMCNotify
+{
+    public class Args
+    {
+        [Argument(ArgumentType.AtMostOnce, HelpText = "The Caption or Title.", ShortName="c")]
+        public string Caption;
+        [Argument(ArgumentType.AtMostOnce, HelpText = "The Message body.", ShortName="m")]
+        public string Message;
+    }
+}
---
+++ XBMCNotify/XBMCNotify/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XBMCNotify
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //args = new string[] { "/c:a","/m:b"};
+            Args a = new Args();
+            if (Unified.CommandLine.Parser.ParseArgumentsWithUsage(args, a))
+            {
+                if (!string.IsNullOrEmpty(a.Caption) && !string.IsNullOrEmpty(a.Message))
+                {
+                    string server = System.Configuration.ConfigurationSettings.AppSettings["Server"];
+                    int port = 9777;
+                    string sPort = System.Configuration.ConfigurationSettings.AppSettings["Port"];
+                    int.TryParse(sPort, out port);
+
+                    bool updateLibrary = false;
+                    bool cleanLibrary = false;
+                    string input = System.Configuration.ConfigurationSettings.AppSettings["UpdateLibrary"];
+                    bool.TryParse(input, out updateLibrary);
+                    input = System.Configuration.ConfigurationSettings.AppSettings["CleanLibrary"];
+                    bool.TryParse(input, out cleanLibrary);
+
+
+                    //XBMC.Default.EventClient ec = new XBMC.Default.EventClient();
+                    if (!XBMC.EventClient.Current.Connected) XBMC.EventClient.Current.Connect(server, port);
+
+                    if (XBMC.EventClient.Current.Connected)
+                    {
+                        XBMC.EventClient.Current.SendNotification(a.Caption, a.Message, XBMC.IconType.ICON_PNG, "sabnzbd");
+                        if(cleanLibrary) XBMC.EventClient.Current.SendAction("CleanLibrary(video)", "");
+                        if(updateLibrary) XBMC.EventClient.Current.SendAction("UpdateLibrary(video)", "");
+                    }
+                }
+            }
+
+        }
+    }
+}
