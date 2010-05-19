eda4708ce2b7e9006b1153e21d18f9d3d7040d44
markus.mcd5@gmail.com <markus.mcd5@gmail.com@94cf2702-d094-11de-a306-d16691e8d855>
2010-05-19 06:05:47 +0000

    Initial commit for PreQueue - Pre-Queue processing script for the 0.6.x release.

---
+++ trunk/PreQueue/PreQueue/ProcessMovie.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace PreQueue
+{
+    class ProcessMovie
+    {
+        private static readonly Logger Logger = new Logger();
+        internal static string GetNzbName(string nzbNameSab)
+        {
+            //Do Movie Name Fixing
+            var patternYear = @"(?<Year>\d{4}(?![a-zA-Z]))";
+
+            string movieName = null;
+            string movieNameOnly = null;
+            int year;
+            Match yearMatch = Regex.Match(nzbNameSab, patternYear);
+
+            if (yearMatch.Success)
+            {
+                var titleSplit = Regex.Split(nzbNameSab, patternYear);
+                movieNameOnly = titleSplit[0].Replace('.', ' ').Replace('_', ' ').TrimEnd(' ', '(', '.', '-', '_', '[');
+                Int32.TryParse(yearMatch.Groups["Year"].Value, out year);
+
+                movieName = movieNameOnly + " (" + year + ")";
+            }
+
+            else
+                movieName = nzbNameSab;
+
+            return movieName;
+        }
+    }
+}
