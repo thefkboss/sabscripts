5ca244a484da1bc34f2bd906b1e0e9c854ad483c
markus.mcd5 <markus.mcd5@94cf2702-d094-11de-a306-d16691e8d855>
2010-06-11 04:28:30 +0000

    Updates to PreQueue

--- trunk/PreQueue/PreQueue/ProcessConsole.cs
+++ trunk/PreQueue/PreQueue/ProcessConsole.cs
@@ -13,7 +13,7 @@ namespace PreQueue
             //Get the type of Console Game (360 (XBLA, DLC, ISO), DS, Wii, etc) - If not found use default "Consoles" Category, store category mapping in config file
             //User can define their own list of strings in nzbName to set the sabCategory to - Matches from top down (should be ordered from most to least strict)
 
-            string sabCategory = "null";
+            string sabCategory = null;
             foreach (ConsoleCategoryMap catMap in Config.ConsoleCategoryMaps)
             {
                 if (nzbName.ToLower().Contains(catMap.ConsoleName.ToLower()))
