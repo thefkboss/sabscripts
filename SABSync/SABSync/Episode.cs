6bc3d71b194e4914fbbd1745f034ca02e3ba55e9
gjohncaruso <gjohncaruso@94cf2702-d094-11de-a306-d16691e8d855>
2010-06-17 19:34:54 +0000

    Interim commit while refactoring Episode to add parsing logic.

--- SABSync/SABSync/Episode.cs
+++ SABSync/SABSync/Episode.cs
@@ -1,9 +1,26 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace SABSync
 {
     public class Episode
     {
+        private const string EpisodePattern = @"
+(?<showName>.*)
+(?:
+  s(?<seasonNumber>\d+)e(?<episodeNumber>\d+)-?e(?<episodeNumber2>\d+)
+| s(?<seasonNumber>\d+)e(?<episodeNumber>\d+)
+|  (?<seasonNumber>\d+)x(?<episodeNumber>\d+)
+|  (?<airDate>\d{4}.\d{2}.\d{2})
+)
+(?:
+  (?<episodeName>.*?)
+  (?<release>
+     (?:hdtv|pdtv|xvid|ws|720p|x264|bdrip|dvdrip|dsr|proper)
+     .*)
+| (?<episodeName>.*)
+)
+";
         public FeedItem FeedItem { get; set; }
         public string ShowName { get; set; }
         public string EpisodeName { get; set; }
@@ -12,6 +29,7 @@ namespace SABSync
         public int EpisodeNumber { get; set; }
         public int EpisodeNumber2 { get; set; }
         public DateTime AirDate { get; set; }
+        public string Release { get; set; }
 
         public bool IsProper
         {
@@ -20,7 +38,7 @@ namespace SABSync
 
         public bool IsDaily
         {
-            get { return SeasonNumber == 0 && EpisodeNumber == 0 && EpisodeNumber2 == 0; }
+            get { return AirDate != DateTime.MinValue; }
         }
 
         public bool IsMulti
@@ -56,6 +74,46 @@ namespace SABSync
         private string GetSeasonEpisode()
         {
             return string.Format("{0}x{1:D2}", SeasonNumber, EpisodeNumber);
+        }
+
+        public static Episode Parse(FeedItem feedItem)
+        {
+            Match match = Regex.Match(feedItem.Title, EpisodePattern, 
+                RegexOptions.IgnorePatternWhitespace | RegexOptions.IgnoreCase);
+            
+            if (!match.Success)
+                return null;
+
+            return new Episode
+            {
+                ShowName = ReplaceSeparatorChars(match.Groups["showName"].Value),
+                SeasonNumber = ParseInt(match.Groups["seasonNumber"].Value),
+                EpisodeNumber = ParseInt(match.Groups["episodeNumber"].Value),
+                EpisodeNumber2 = ParseInt(match.Groups["episodeNumber2"].Value),
+                EpisodeName = ReplaceSeparatorChars(match.Groups["episodeName"].Value),
+                Release = ReplaceSeparatorChars(match.Groups["release"].Value)
+            };
+        }
+
+        private static string ReplaceSeparatorChars(string s)
+        {
+            if (s == null) return string.Empty;
+            return s.Replace('.', ' ').Replace('-', ' ').Replace('_', ' ').Trim();
+        }
+
+        private static int ParseInt(string s)
+        {
+            int i;
+            int.TryParse(s, out i);
+            return i;
+        }
+
+        private static DateTime ParseAirDate(string s)
+        {
+            DateTime d;
+            if (DateTime.TryParse(ReplaceSeparatorChars(s).Replace(' ', '-'), out d))
+                return d;
+            return DateTime.MinValue;
         }
     }
 }
