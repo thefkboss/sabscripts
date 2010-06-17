e3fb922f156c901f8c01ab94dc6cb0238b68f583
gjohncaruso <gjohncaruso@94cf2702-d094-11de-a306-d16691e8d855>
2010-06-17 19:34:54 +0000

    Interim commit while refactoring Episode to add parsing logic.

---
+++ trunk/SABSync/SABSync.Tests/FeedTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using NUnit.Framework;
+
+namespace SABSync.Tests
+{
+    [TestFixture]
+    internal class FeedTest : AssertionHelper
+    {
+        [Test]
+        public void Test1()
+        {
+            XElement rssFeed = XElement.Load(@"nzbs-rss.xml");
+
+            IEnumerable<string> items =
+                from item in rssFeed.Elements("channel").Elements("item")
+                select (string) item.Element("title");
+
+            Console.WriteLine(items.Count());
+            foreach (string o in items)
+            {
+                Console.WriteLine(o);
+            }
+        }
+    }
+}
